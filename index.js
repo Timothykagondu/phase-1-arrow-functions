01b53ef8861ed2e0b2b1af664610b85f6ce13046
Timothy <timothy.2k21@gmail.com>
2022-08-21 12:49:05 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (a =100, b =2000){
+    return 2000 / 100;
+};
+
+const square = (a = 2, b = 2) => {
+    return a ** b ;
+};
+
+const add = (a, b) => {
+    return a + b ;
+};
